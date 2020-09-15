42e9e8f51e31e6684280c9aeb8b6c4d74d33eaab
Cameron Howell <crh0043@uah.edu>
2020-09-15 00:10:55 -0500

    Changed functionality of weatherbot

---
+++ bot.js
@@ -0,0 +1,40 @@
+const { Client } = require("discord.js");
+const { config } = require("dotenv");
+const { token } = require("./auth.json");
+
+const client = new Client({
+	disableEveryone: true,
+});
+
+config({
+	path: __dirname + "/.env",
+});
+
+client.on("ready", () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+	client.user.setPresence({
+		activity: {
+			name: "EVERYTHING",
+			type: "WATCHING",
+		},
+		status: "online",
+	});
+	setInterval(function () {
+		if (
+			client.guilds.cache
+				.get("690763269543034902")
+				.members.cache.get("696937315804643362")
+				.presence.equals("offline")
+		) {
+			client.channels.cache
+				.get("690763269543034905")
+				.message.send("@omni-bot is offline.");
+		} else {
+			client.channels.cache
+				.get("690763269543034905")
+				.message.send("@omni-bot is online");
+		}
+	}, 5 * 1000);
+});
+
+client.login(token);
